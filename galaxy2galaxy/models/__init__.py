12ab706657c7d6fec58fa73c7da0b6456d4c52c9
Francois <francois@orioncloud.fr>
2019-08-04 00:10:32 -0700

    Adds executables

---
+++ galaxy2galaxy/models/__init__.py
@@ -0,0 +1,8 @@
+"""Models defined in G2G. Imports here force registration."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import six
+
+from galaxy2galaxy.models import autoencoders
