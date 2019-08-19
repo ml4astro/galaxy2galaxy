97af89d4d0b0eb74eb812548f40d4429506f6979
Francois <francois@orioncloud.fr>
2019-08-18 23:10:24 -0700

    Adds corrected version of data readder

--- galaxy2galaxy/data_generators/astroimage_utils.py
+++ galaxy2galaxy/data_generators/astroimage_utils.py
@@ -12,6 +12,136 @@ from tensor2tensor.utils import metrics
 
 import tensorflow as tf
 
+
+
+from tensorflow.contrib.slim.python.slim.data import data_decoder
+from tensorflow.contrib.slim.python.slim.data.tfexample_decoder import ItemHandler
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import sparse_tensor
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import map_fn
+from tensorflow.python.ops import image_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import parsing_ops
+from tensorflow.python.ops import sparse_ops
+class Image(ItemHandler):
+  """ Item Handler copied from
+  https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/slim/python/slim/data/tfexample_decoder.py
+  There is a bug in tf 1.14 that prevents reading raw images with arbitry numbers
+  of channels. This is has a fix for that.
+  TODO: Switch back to upstream data handler code at next release
+  """
+
+  def __init__(self,
+               image_key=None,
+               format_key=None,
+               shape=None,
+               channels=3,
+               dtype=dtypes.uint8,
+               repeated=False,
+               dct_method=''):
+    """Initializes the image.
+    Args:
+      image_key: the name of the TF-Example feature in which the encoded image
+        is stored.
+      format_key: the name of the TF-Example feature in which the image format
+        is stored.
+      shape: the output shape of the image as 1-D `Tensor`
+        [height, width, channels]. If provided, the image is reshaped
+        accordingly. If left as None, no reshaping is done. A shape should
+        be supplied only if all the stored images have the same shape.
+      channels: the number of channels in the image.
+      dtype: images will be decoded at this bit depth. Different formats
+        support different bit depths.
+          See tf.image.decode_image,
+              tf.io.decode_raw,
+      repeated: if False, decodes a single image. If True, decodes a
+        variable number of image strings from a 1D tensor of strings.
+      dct_method: An optional string. Defaults to empty string. It only takes
+        effect when image format is jpeg, used to specify a hint about the
+        algorithm used for jpeg decompression. Currently valid values
+        are ['INTEGER_FAST', 'INTEGER_ACCURATE']. The hint may be ignored, for
+        example, the jpeg library does not have that specific option.
+    """
+    if not image_key:
+      image_key = 'image/encoded'
+    if not format_key:
+      format_key = 'image/format'
+
+    super(Image, self).__init__([image_key, format_key])
+    self._image_key = image_key
+    self._format_key = format_key
+    self._shape = shape
+    self._channels = channels
+    self._dtype = dtype
+    self._repeated = repeated
+    self._dct_method = dct_method
+
+  def tensors_to_item(self, keys_to_tensors):
+    """See base class."""
+    image_buffer = keys_to_tensors[self._image_key]
+    image_format = keys_to_tensors[self._format_key]
+
+    if self._repeated:
+      return map_fn.map_fn(lambda x: self._decode(x, image_format),
+                           image_buffer, dtype=self._dtype)
+    else:
+      return self._decode(image_buffer, image_format)
+
+  def _decode(self, image_buffer, image_format):
+    """Decodes the image buffer.
+    Args:
+      image_buffer: The tensor representing the encoded image tensor.
+      image_format: The image format for the image in `image_buffer`. If image
+        format is `raw`, all images are expected to be in this format, otherwise
+        this op can decode a mix of `jpg` and `png` formats.
+    Returns:
+      A tensor that represents decoded image of self._shape, or
+      (?, ?, self._channels) if self._shape is not specified.
+    """
+
+    def decode_image():
+      """Decodes a image based on the headers."""
+      return math_ops.cast(
+          image_ops.decode_image(image_buffer, channels=self._channels),
+          self._dtype)
+
+    def decode_jpeg():
+      """Decodes a jpeg image with specified '_dct_method'."""
+      return math_ops.cast(
+          image_ops.decode_jpeg(
+              image_buffer,
+              channels=self._channels,
+              dct_method=self._dct_method), self._dtype)
+
+    def check_jpeg():
+      """Checks if an image is jpeg."""
+      # For jpeg, we directly use image_ops.decode_jpeg rather than decode_image
+      # in order to feed the jpeg specify parameter 'dct_method'.
+      return control_flow_ops.cond(
+          image_ops.is_jpeg(image_buffer),
+          decode_jpeg,
+          decode_image,
+          name='cond_jpeg')
+
+    def decode_raw():
+      """Decodes a raw image."""
+      return parsing_ops.decode_raw(image_buffer, out_type=self._dtype)
+
+    pred_fn_pairs = [(math_ops.logical_or(
+        math_ops.equal(image_format, 'raw'),
+        math_ops.equal(image_format, 'RAW')), decode_raw)]
+    image = control_flow_ops.case(
+        pred_fn_pairs, default=check_jpeg, exclusive=True)
+
+    image.set_shape([None, None, self._channels])
+    if self._shape is not None:
+      image = array_ops.reshape(image, self._shape)
+
+    return image
+
+
 class AstroImageProblem(problem.Problem):
   """Base class for image problems on astronomical images.
   """
@@ -192,7 +322,7 @@ class AstroImageProblem(problem.Problem):
             data_fields['attrs/'+k] = tf.FixedLenFeature([], tf.float32, -1)
 
     data_items_to_decoders = {
-        "inputs": tf.contrib.slim.tfexample_decoder.Image(
+        "inputs": Image( # TODO: switch back to tf.contrib.slim.tfexample_decoder.
                 image_key="image/encoded",
                 format_key="image/format",
                 channels=self.num_bands,
