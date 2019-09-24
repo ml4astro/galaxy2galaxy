84371ea84baf1bdf6a68704983e91471a0dde9ca
Francois <fr.eiffel@gmail.com>
2019-09-24 10:57:00 -0700

    Fixes the embedding

--- galaxy2galaxy/models/autoencoders_utils.py
+++ galaxy2galaxy/models/autoencoders_utils.py
@@ -49,7 +49,7 @@ def autoencoder_body(self, features):
     if len(labels.shape) == 5:
       labels = time_to_channels(labels)
     shape = common_layers.shape_list(labels)
-    x = self.embed(x)
+    x = self.embed(labels)
     target_codes = x
     if shape[2] == 1:
       self.is1d = True
