0089b6104e61db5bbf5491767f51629cfcd8a2d7
Thomas Sainrat <thomas.sainrat@student-cs.fr>
2021-03-08 11:43:49 +0000

    Fix task_id

--- galaxy2galaxy/data_generators/candels.py
+++ galaxy2galaxy/data_generators/candels.py
@@ -759,8 +759,9 @@ class Img2imgCandelsGoodsMultires(astroimage_utils.AstroImageProblem):
     ''' Create a subcat containing only the galaxies (in every filters) of the current field'''
     sub_cat = all_cat[np.where(np.isin(list(all_cat["FIELD_1"]),["GDS","GDN","EGS","COSMOS","UDS"]))]
     assert(task_id > -1)
-    indexes = list(range(task_id*3*p.example_per_shard,
-                  min((task_id*3+1)*p.example_per_shard, len(sub_cat))))
+    n_shards = self.dataset_splits()[0]["shards"] + self.dataset_splits()[1]["shards"]
+    indexes = list(range(task_id*len(sub_cat)/n_shards*p.example_per_shard,
+                  min((task_id+1)*len(sub_cat)/n_shards*p.example_per_shard, len(sub_cat))))
     sub_cat = sub_cat[indexes]
 
     ''' Loop on all the galaxies of the field '''
